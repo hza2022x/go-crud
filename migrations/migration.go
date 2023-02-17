6128696d68a85415d0bbddcc7a8d2e958ba5bf15
wangxiaoming <wangxiaoming@joyy.com>
2023-02-17 10:12:29 +0800

    update local config

--- migrations/migration.go
+++ migrations/migration.go
@@ -1,26 +1,19 @@
 package migrations
 
-import (
-	"log"
-
-	"github.com/go-gormigrate/gormigrate/v2"
-	"github.com/yodfhafx/go-crud/config"
-)
-
 func Migrate() {
-	db := config.GetDB()
-	m := gormigrate.New(
-		db,
-		gormigrate.DefaultOptions,
-		[]*gormigrate.Migration{
-			m1675671925CreateArticlesTable(),
-			m1675745269CreateCategoriesTable(),
-			m1675747773AddCategoryIDToArticles(),
-			m1675752464CreateUsersTable(),
-		},
-	)
-
-	if err := m.Migrate(); err != nil {
-		log.Fatalf("Could not migrate: %v", err)
-	}
+	//db := config.GetDB()
+	//m := gormigrate.New(
+	//	db,
+	//	gormigrate.DefaultOptions,
+	//	[]*gormigrate.Migration{
+	//		m1675671925CreateArticlesTable(),
+	//		m1675745269CreateCategoriesTable(),
+	//		m1675747773AddCategoryIDToArticles(),
+	//		m1675752464CreateUsersTable(),
+	//	},
+	//)
+	//
+	//if err := m.Migrate(); err != nil {
+	//	log.Fatalf("Could not migrate: %v", err)
+	//}
 }
